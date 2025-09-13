c8be7025e30ef401830f885fc4902005bfa52f81
phthien-vnm <phthien@vinamilk.com.vn>
2025-09-13 10:57:45 +0700

    animation when processing (5)

--- components/step_widget.py
+++ components/step_widget.py
@@ -71,8 +71,8 @@ class StepWidget(QFrame):
 
         # Create status icon labels
         self.status_icon = QLabel("⏳")
-        self.status_icon.setStyleSheet("font-size: 14px;")
-        self.status_icon.setFixedSize(16, 16)
+        self.status_icon.setStyleSheet("font-size: 12px;")
+        self.status_icon.setMinimumSize(16, 16)
         self.status_icon.setAlignment(Qt.AlignmentFlag.AlignCenter)
 
         self.spin_icon = RotatingLabel("⚙")
